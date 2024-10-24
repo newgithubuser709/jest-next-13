03384f03d927a36150ac06396ec03172afbbd173
M.Haseeb <131981512+newgithubuser709@users.noreply.github.com>
2024-10-24 06:39:43 -0700

    Add files via upload

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+// jest.config.js
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+const customJestConfig = {
+  testEnvironment: "jsdom",
+  moduleNameMapper: {
+    "^@/components/(.*)$": "<rootDir>/components/$1",
+  },
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+};
+
+module.exports = createJestConfig(customJestConfig);
